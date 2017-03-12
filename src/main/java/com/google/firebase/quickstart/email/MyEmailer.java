5113b46244c787581da9650554bda05d604a4215
Joe Kutner <jpkutner@gmail.com>
2017-03-12 12:58:16 -0500

    Convert smtp config

--- src/main/java/com/google/firebase/quickstart/email/MyEmailer.java
+++ src/main/java/com/google/firebase/quickstart/email/MyEmailer.java
@@ -17,13 +17,10 @@ package com.google.firebase.quickstart.email;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.SimpleEmail;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,38 +29,15 @@ import java.util.Map;
  */
 public class MyEmailer {
 
-    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-
-    private static OkHttpClient client = new OkHttpClient();
-
     public static void sendNotificationEmail(String emailAddr, String uid, String postId) {
         System.out.println("sendNotificationEmail: " + emailAddr);
 
         try {
-            String smtpServer = System.getenv("MAILGUN_SMTP_SERVER");
+            String smtpServer = System.getenv("SPARKPOST_SMTP_HOST");
             if (smtpServer != null) {
-//                String url = "https://api:" + apiKey + "@api.mailgun.net/v2/" + domain + "/messages";
-//
-//                String json = "{" +
-//                    "'from':'test@example.com'," +
-//                    "'to': '" + email + "'," +
-//                    "'subject': 'New post!'," +
-//                    "'text': 'This is a notfication from your Heroku app. There was a new post received by Firebase.'" +
-//                    "}";
-//
-//                RequestBody body = RequestBody.create(JSON, json);
-//                Request request = new Request.Builder()
-//                    .url(url)
-//                    .post(body)
-//                    .build();
-//                Response response = client.newCall(request).execute();
-//                System.out.println("[mailgun] " + response.message());
-//                System.out.println("[mailgun] " + response.code());
-//                System.out.println("[mailgun] " + response.body());
-
-                Integer smtpPort = Integer.valueOf(System.getenv("MAILGUN_SMTP_PORT"));
-                String smtpUsername = System.getenv("MAILGUN_SMTP_LOGIN");
-                String smtpPassword = System.getenv("MAILGUN_SMTP_PASSWORD");
+                Integer smtpPort = Integer.valueOf(System.getenv("SPARKPOST_SMTP_PORT"));
+                String smtpUsername = System.getenv("SPARKPOST_SMTP_USERNAME");
+                String smtpPassword = System.getenv("SPARKPOST_SMTP_PASSWORD");
 
                 Email email = new SimpleEmail();
                 email.setHostName(smtpServer);
@@ -76,7 +50,7 @@ public class MyEmailer {
                 email.addTo(emailAddr);
                 String result = email.send();
 
-                System.out.println("[mailgun] " + result);
+                System.out.println("[smtp] " + result);
 
                 // Save the date of the last notification sent
                 Map<String, Object> update = new HashMap<String, Object>();
