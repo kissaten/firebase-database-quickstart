9f7b1136c30b5e029f26c973b0d827b40e261781
Joe Kutner <jpkutner@gmail.com>
2017-03-12 11:27:13 -0500

    Use smtp instead of http for email

--- src/main/java/com/google/firebase/quickstart/email/MyEmailer.java
+++ src/main/java/com/google/firebase/quickstart/email/MyEmailer.java
@@ -17,7 +17,11 @@ package com.google.firebase.quickstart.email;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
-import okhttp3.*;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -32,31 +36,47 @@ public class MyEmailer {
 
     private static OkHttpClient client = new OkHttpClient();
 
-    public static void sendNotificationEmail(String email, String uid, String postId) {
-        System.out.println("sendNotificationEmail: " + email);
+    public static void sendNotificationEmail(String emailAddr, String uid, String postId) {
+        System.out.println("sendNotificationEmail: " + emailAddr);
 
         try {
-            String apiKey = System.getenv("MAILGUN_API_KEY");
-            String domain = System.getenv("MAILGUN_DOMAIN");
-            if (apiKey != null) {
-                String url = "https://api:" + apiKey + "@api.mailgun.net/v2/" + domain + "/messages";
+            String smtpServer = System.getenv("MAILGUN_SMTP_SERVER");
+            if (smtpServer != null) {
+//                String url = "https://api:" + apiKey + "@api.mailgun.net/v2/" + domain + "/messages";
+//
+//                String json = "{" +
+//                    "'from':'test@example.com'," +
+//                    "'to': '" + email + "'," +
+//                    "'subject': 'New post!'," +
+//                    "'text': 'This is a notfication from your Heroku app. There was a new post received by Firebase.'" +
+//                    "}";
+//
+//                RequestBody body = RequestBody.create(JSON, json);
+//                Request request = new Request.Builder()
+//                    .url(url)
+//                    .post(body)
+//                    .build();
+//                Response response = client.newCall(request).execute();
+//                System.out.println("[mailgun] " + response.message());
+//                System.out.println("[mailgun] " + response.code());
+//                System.out.println("[mailgun] " + response.body());
 
-                String json = "{" +
-                    "'from':'test@example.com'," +
-                    "'to': '" + email + "'," +
-                    "'subject': 'New post!'," +
-                    "'text': 'This is a notfication from your Heroku app. There was a new post received by Firebase.'" +
-                    "}";
+                Integer smtpPort = Integer.valueOf(System.getenv("MAILGUN_SMTP_PORT"));
+                String smtpUsername = System.getenv("MAILGUN_SMTP_LOGIN");
+                String smtpPassword = System.getenv("MAILGUN_SMTP_PASSWORD");
 
-                RequestBody body = RequestBody.create(JSON, json);
-                Request request = new Request.Builder()
-                    .url(url)
-                    .post(body)
-                    .build();
-                Response response = client.newCall(request).execute();
-                System.out.println("[mailgun] " + response.message());
-                System.out.println("[mailgun] " + response.code());
-                System.out.println("[mailgun] " + response.body());
+                Email email = new SimpleEmail();
+                email.setHostName(smtpServer);
+                email.setSmtpPort(smtpPort);
+                email.setAuthenticator(new DefaultAuthenticator(smtpUsername, smtpPassword));
+                email.setSSLOnConnect(true);
+                email.setFrom("test@example.com");
+                email.setSubject("New post!");
+                email.setMsg("This is a notfication from your Heroku app. There was a new post received by Firebase.");
+                email.addTo(emailAddr);
+                String result = email.send();
+
+                System.out.println("[mailgun] " + result);
 
                 // Save the date of the last notification sent
                 Map<String, Object> update = new HashMap<String, Object>();
@@ -65,7 +85,7 @@ public class MyEmailer {
 
                 FirebaseDatabase.getInstance().getReference().updateChildren(update);
             }
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             System.out.println("Whoops! Unable to send email.");
             ex.printStackTrace();
         }
