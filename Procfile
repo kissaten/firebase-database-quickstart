5b0d4d4edbe5c925bda29a36e95e07bbfe891e28
Joe Kutner <jpkutner@gmail.com>
2017-03-12 10:54:44 -0500

    Added Heroku files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/database/bin/database
---
+++ src/main/java/com/google/firebase/quickstart/servlet/MainServlet.java
@@ -0,0 +1,8 @@
+package com.google.firebase.quickstart.servlet;
+
+/**
+ * @author Joe Kutner on 3/12/17.
+ *         Twitter: @codefinger
+ */
+public class MainServlet {
+}
